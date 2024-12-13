c50405015042fe9fb1d4eaa69820ef17b1f125ca
Kamran Choudhry <intractable.truth@gmail.com>
2024-12-12 19:30:55 -0500

    EDIT: tweak providers

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import { ConnectWallet } from "./components/ConnectWallet";
 
-export default function Home() {
+export default function App() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen gap-4">
       <ConnectWallet />
