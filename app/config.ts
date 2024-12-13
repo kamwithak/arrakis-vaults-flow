532b57c57b625a7e49149bb1328eaa9f7c9aa059
Kamran Choudhry <intractable.truth@gmail.com>
2024-12-12 19:22:04 -0500

    INIT: configt.ts wagmi

---
+++ app/config.ts
@@ -0,0 +1,21 @@
+import { http, createConfig } from 'wagmi'
+import { arbitrum, base, mainnet, optimism } from 'wagmi/chains'
+import { injected, metaMask, safe } from 'wagmi/connectors'
+
+// const projectId = '<WALLETCONNECT_PROJECT_ID>' // TODO: point to .env
+
+export const config = createConfig({
+  chains: [mainnet, base],
+  connectors: [
+    injected(),
+    // walletConnect({ projectId }),
+    metaMask(),
+    safe(),
+  ],
+  transports: {
+    [mainnet.id]: http(),
+    [optimism.id]: http(),
+    [arbitrum.id]: http(),
+    [base.id]: http(),
+  },
+})
