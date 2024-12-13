2e4f945d0e6c207e5a45e27d3f427b627887e6cc
Kamran Choudhry <intractable.truth@gmail.com>
2024-12-12 20:24:07 -0500

    EDIT: stndized styling and layout tweak

--- app/components/Account.tsx
+++ app/components/Account.tsx
@@ -9,10 +9,10 @@ export function Account() {
   const { data: ensAvatar } = useEnsAvatar({ name: ensName! })
 
   return (
-    <div>
+    <div className='flex flex-col items-center gap-6'>
       {ensAvatar && <img alt="ENS Avatar" src={ensAvatar} />}
       {address && <div>{ensName ? `${ensName} (${address})` : address}</div>}
-      <button onClick={() => disconnect()}>Disconnect</button>
+      <button onClick={() => disconnect()} className='bg-blue-600 hover:bg-blue-500 text-white px-4 py-2 rounded-md'>Disconnect</button>
     </div>
   )
 }
