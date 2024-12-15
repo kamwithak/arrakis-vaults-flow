55e363bde3a6d44f556503b7a2db5f88b44b285e
Kamran Choudhry <intractable.truth@gmail.com>
2024-12-15 11:06:23 -0500

    EDIT: styling tweak

--- app/components/Account.tsx
+++ app/components/Account.tsx
@@ -1,18 +1,23 @@
-'use client'
+"use client";
 
-import { useAccount, useDisconnect, useEnsAvatar, useEnsName } from 'wagmi'
+import { useAccount, useDisconnect, useEnsAvatar, useEnsName } from "wagmi";
 
 export function Account() {
-  const { address } = useAccount()
-  const { disconnect } = useDisconnect()
-  const { data: ensName } = useEnsName({ address })
-  const { data: ensAvatar } = useEnsAvatar({ name: ensName! })
+  const { address } = useAccount();
+  const { disconnect } = useDisconnect();
+  const { data: ensName } = useEnsName({ address });
+  const { data: ensAvatar } = useEnsAvatar({ name: ensName! });
 
   return (
-    <div className='flex flex-col items-center gap-6'>
+    <div className="flex flex-col items-center gap-6">
       {ensAvatar && <img alt="ENS Avatar" src={ensAvatar} />}
-      {address && <div>{ensName ? `${ensName} (${address})` : address}</div>}
-      <button onClick={() => disconnect()} className='bg-blue-600 hover:bg-blue-500 text-white px-4 py-2 rounded-md'>Disconnect</button>
+      {address && <h1 className="text-white">{ensName ? `${ensName} (${address})` : address}</h1>}
+      <button
+        onClick={() => disconnect()}
+        className="bg-blue-600 hover:bg-blue-500 text-white px-4 py-2 rounded-md"
+      >
+        Disconnect
+      </button>
     </div>
-  )
+  );
 }
