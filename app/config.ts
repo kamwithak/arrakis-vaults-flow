d08b45bc6432097437068ee3b557d1142046a8e8
Kamran Choudhry <intractable.truth@gmail.com>
2024-12-16 15:52:27 -0500

    EDIT: update wagi config for rainbowkit integration

--- app/config.ts
+++ app/config.ts
@@ -1,21 +1,23 @@
-import { http, createConfig } from "wagmi";
-import { arbitrum, base, mainnet, optimism } from "wagmi/chains";
-import { injected, metaMask, safe } from "wagmi/connectors";
+import {
+  getDefaultConfig,
+} from '@rainbow-me/rainbowkit'
+import {
+  mainnet,
+  sepolia,
+} from 'wagmi/chains'
+import { http } from 'wagmi'
 
-// const projectId = '<WALLETCONNECT_PROJECT_ID>' // TODO: point to .env
+if (!process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID) {
+  throw new Error('Missing NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID')
+}
 
-export const config = createConfig({
-  chains: [mainnet, base],
-  connectors: [
-    injected(),
-    // walletConnect({ projectId }),
-    metaMask(),
-    safe(),
-  ],
+export const config = getDefaultConfig({
+  appName: 'Arrakis Vault Application',
+  projectId: process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID,
+  chains: [mainnet, sepolia],
   transports: {
     [mainnet.id]: http(),
-    [optimism.id]: http(),
-    [arbitrum.id]: http(),
-    [base.id]: http(),
+    [sepolia.id]: http(),
   },
-});
+  ssr: true
+})
